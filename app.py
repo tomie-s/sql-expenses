42aff69fc3b7421243e7202f74771a684ee5cdc2
Tomi Salami <salami.tomie@gmail.com>
2025-04-24 15:57:29 -0400

    base logic for launching the application and Qt window added

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 # App Design
+from PyQt6.QtWidgets import QWidget, QLabel, QPushButton, QLineEdit, QComboBox, QDateEdit, QTableWidget, QVBoxLayout, \
+    QHBoxLayout, QMessageBox, QTableWidgetItem, QHeaderView
+from PyQt6.QtCore import Qt, QDate
+
+
+# QWidget represents the main window of the application
+# Alternative is QMainWindow
+class ExpenseApp(QWidget):
+    def __init__(self):
+        super().__init__()  # Initialize the parent class QWidget
+        self.settings()  # Call the settings method to set up the window properties
+
+    def settings(self):
+        self.setWindowTitle("Expense Tracker App")
+        self.setGeometry(300, 300, 800, 600)
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Running the application
+import sys
+from PyQt6.QtWidgets import QApplication, QMessageBox
+from app import ExpenseApp
+
+
+def main():
+    app = QApplication(sys.argv)  # Create a QApplication instance
+    window = ExpenseApp()  # Create an instance of the ExpenseApp class
+    window.show()  # Show the main window
+    sys.exit(app.exec())  # Start the event loop and exit when done
+
+
+if __name__ == "__main__":
+    main()  # Call the main function to run the application
