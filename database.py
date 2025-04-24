052ef36cd28c5450890bc5a5c2ceab9abfc684c1
Tomi Salami <salami.tomie@gmail.com>
2025-04-24 17:26:08 -0400

    added code for database setup

--- database.py
+++ database.py
@@ -1,1 +1,22 @@
 # All SQL related functions
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
+
+def init_db(db_name):
+    database = QSqlDatabase.addDatabase("QSQLITE")
+    database.setDatabaseName(db_name)
+    if not database.open():
+        print("Unable to open the database.")
+        return False
+    
+    query = QSqlQuery()
+    query.exec("""
+        CREATE TABLE IF NOT EXISTS expenses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT NOT NULL,
+            category TEXT NOT NULL,
+            amount REAL NOT NULL,
+            description TEXT
+        )
+    """)
+
+    return True
--- main.py
+++ main.py
@@ -2,10 +2,17 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMessageBox
 from app import ExpenseApp
+from database import init_db
 
 
 def main():
     app = QApplication(sys.argv)  # Create a QApplication instance
+
+    if not init_db("expenseapp.db"):
+        # If the database initialization fails, show an error message and exit
+        QMessageBox.critical(None, "Error", "Failed to load the database.")
+        sys.exit(1)
+
     window = ExpenseApp()  # Create an instance of the ExpenseApp class
     window.show()  # Show the main window
     sys.exit(app.exec())  # Start the event loop and exit when done
